8e50ad8566b8f83754dd9411bcfa18909190140a
riccardoEspositoXhpo <espositoriccardo101@gmail.com>
2024-09-15 22:46:35 +0200

    commit time!

--- post-install/fonts/install.sh
+++ post-install/fonts/install.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-# source $LINUX_TOOLKIT_UTILITIES
+source $LINUX_TOOLKIT_UTILITIES
 
-# script_init
-# install_dependencies
-# script_exit
+script_init
+install_dependencies
+
+FONT_NAME="JetBrainsMono"
+info "Downloading $FONT_NAME font..."
+# Fetch the latest JetBrainsMono.zip download URL
+url=$(curl -s https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep "browser_download_url" | grep "JetBrainsMono.zip" | cut -d '"' -f 4)
+
+# Download the latest JetBrainsMono.zip
+curl -L -o /tmp/$FONT_NAME.zip "$url"
+
+info "Extracting $FONT_NAME to local fonts dir"
+unzip /tmp/$FONT_NAME.zip -D /tmp/$FONT_NAME
+cp /tmp/$FONT_NAME "$HOME/.local/share/fonts/"
+
+info "Refreshing font cache"
+fd-cache
+
+
+script_exit
